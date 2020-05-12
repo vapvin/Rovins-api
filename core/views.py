8e6fd9964ef3134c974b3f78104778c5d1ad26c9
vapvin <tpehdj12@daum.net>
2020-05-12 10:23:50 +0900

    Let's Started Make Apis

--- core/views.py
+++ core/views.py
@@ -3,6 +3,4 @@ from django.http import HttpResponse
 from rooms.models import Room
 
 def list_rooms(request):
-    data = serializers.serialize('json', Room.objects.all())
-    response = HttpResponse(content=data)
-    return response
+    pass
