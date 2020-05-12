c0fc8714f7fd3e470b4398d211437e5844fe72f2
vapvin <tpehdj12@daum.net>
2020-05-12 11:14:35 +0900

    Class Base View Api

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView
+from .models import Room
+from .serializers import RoomSerializer
 
-# Create your views here.
+
+class ListRoomsView(ListAPIView):
+
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
