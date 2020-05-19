740f3b9fcd25b68ad07d91b53c7b8cec088966ae
vapvin <tpehdj12@daum.net>
2020-05-19 19:05:16 +0900

    Get edit , put adding

--- rooms/views.py
+++ rooms/views.py
@@ -31,10 +31,31 @@ class SeeRoomView(RetrieveAPIView):
 
 
 class RoomView(APIView):
-    def get(self, request, pk):
+    def get_room(self, pk):
         try:
             room = Room.objects.get(pk=pk)
-            serializer = ReadRoomSerializer(room).data
-            return Response(serializer)
-        except Room.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
+            return room
+                    except Room.DoesNotExist:
+                        return None
+
+                def get(self, request, pk):
+                    room = self.get_room(pk)
+                    if room is not None:
+                        serializer = ReadRoomSerializer(room).data
+                        return Response(serializer)
+                    else:
+                        return Response(status=status.HTTP_404_NOT_FOUND)
+
+      def put(self, request, pk):
+            room = self.get_room(pk)
+            if room is not None:
+                if room.user != request.user:
+                    return Response(status=status.HTTP_403_FORBIDDEN)
+                serializer = WriteRoomSerializer(room, data=request.data, partial=True)
+                if serializer.is_valid():
+                    serializer.save()
+                else:
+                    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+                return Response()
+            else:
+                return Response(status=status.HTTP_404_NOT_FOUND)
