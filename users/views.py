c2b2ca78ce7dd2d7a7a7e8cd63d4cd261f2dd550
vapvin <tpehdj12@daum.net>
2020-05-27 00:17:57 +0900

    Put

--- users/views.py
+++ users/views.py
@@ -1,18 +1,25 @@
-from django.shortcuts import render
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import api_view
 from rest_framework.views import api_view
 from rest_framework import status
 from .models import User
-from .serializers import ReadUserSerializer
+from .serializers import ReadUserSerializer, WriteUserSerializer
 
 class MeView(APIView):
+
+    permission_classes = [IsAuthenticated]
+    
     def get(self, request):
-        if request.user.is_authenticated:
-            return Response(ReadUserSerializer(request.user).data)
+        return Response(ReadUserSerializer(request.user).data)
 
     def put(self, request):
-        pass
+        serializer = WriteUserSerializer(request.user, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response()
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(["GET"])
 def user_detail(request, pk):
@@ -21,4 +28,4 @@ def user_detail(request, pk):
         return Response(ReadUserSerializer(user).data)
     except User.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
-        
+
