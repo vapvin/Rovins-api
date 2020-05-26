7746b5e20e2249f44abc9de3af9f9f24302f6ca8
vapvin <tpehdj12@daum.net>
2020-05-27 00:11:12 +0900

    User Views

--- users/serializers.py
+++ users/serializers.py
@@ -1,8 +1,20 @@
 from rest_framework import serializers
 from .models import User
 
+class RelatedUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = (
+            "username",
+            "first_name",
+            "last_name",
+            "email",
+            "avatar",
+            "superhost",
+        )
 
-class UserSerializer(serializers.ModelSerializer):
+
+class ReadUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         exclude = (
