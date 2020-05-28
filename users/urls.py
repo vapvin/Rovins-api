0e27f497b66e313ab1c31d7eceeaba6861ea8bdd
vapvin <tpehdj12@daum.net>
2020-05-28 22:44:17 +0900

    Debug Comment

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,10 @@
-from django.urls import path
-from . import views
+# from django.urls import path
+# from . import views
 
-app_name = "users"
+# app_name = "users"
 
-urlpatterns = [
-    path("me/", views.MeView.as_view()),
-    path("me/favs/", views.FavsView.as_view()),
-    path("<int:pk>/", views.user_detail),
-]
+# urlpatterns = [
+#     path("me/", views.MeView.as_view()),
+#     path("me/favs/", views.FavsView.as_view()),
+#     path("<int:pk>/", views.user_detail),
+# ]
