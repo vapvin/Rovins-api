376c6f5f257b25311714b51dd7a46130bc508aa4
vapvin <tpehdj12@daum.net>
2020-05-28 22:44:37 +0900

    How to fix them

--- users/models.py
+++ users/models.py
@@ -1,14 +1,14 @@
-from django.contrib.auth.models import AbstractUser
-from django.db import models
+# from django.contrib.auth.models import AbstractUser
+# from django.db import models
 
 
-class User(AbstractUser):
+# class User(AbstractUser):
 
-    avatar = models.ImageField(upload_to="avatars", blank=True)
-    superhost = models.BooleanField(default=False)
-    favs = models.ManyToManyField("rooms.Room", related_name="favs")
+#     avatar = models.ImageField(upload_to="avatars", blank=True)
+#     superhost = models.BooleanField(default=False)
+#     favs = models.ManyToManyField("rooms.Room", related_name="favs")
 
-    def room_count(self):
-        return self.rooms.count()
+#     def room_count(self):
+#         return self.rooms.count()
 
-    room_count.short_description = "Room Count"
+#     room_count.short_description = "Room Count"
