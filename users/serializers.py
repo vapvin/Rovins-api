6837ecf70cb7f37e59f04dc5a0924e0fdf0cb5fe
vapvin <tpehdj12@daum.net>
2020-05-30 03:55:29 +0900

    My Apis Awesome!

--- users/serializers.py
+++ users/serializers.py
@@ -2,7 +2,20 @@ from rest_framework import serializers
 from .models import User
 
 
-class UserSerializer(serializers.ModelSerializer):
+class RelatedUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = (
+            "username",
+            "first_name",
+            "last_name",
+            "email",
+            "avatar",
+            "superhost",
+        )
+
+
+class ReadUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         exclude = (
@@ -15,3 +28,13 @@ class UserSerializer(serializers.ModelSerializer):
             "is_active",
             "date_joined",
         )
+
+
+class WriteUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ("username", "first_name", "last_name", "email")
+
+    def validate_first_name(self, value):
+        print(value)
+        return value.upper()
